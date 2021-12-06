49b5af7e2a7ede7d07302d27d3069cee0c8a51d7
THC <88347509+THCVP@users.noreply.github.com>
2021-12-05 23:43:58 -0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'THC'
+
+description 'Custom Crosshair URL'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'client/data.lua',
+	'client/main.lua'
+}
+
+files {
+	'html/index.html',
+	'html/style.css',
+	'html/main.js'
+}
